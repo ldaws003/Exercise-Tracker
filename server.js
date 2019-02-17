f46b5f202fa2089af809c3c8cdf51c188de66d9d
ldaws003 <ldaws003@fiu.edu>
2019-02-17 01:05:44 -0500

    added in mongoose

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('mongodb');
+const mongoose = require('mongoose');
 const ObjectID = require('mongodb').ObjectID;
 const passport = require('passport');
 const passportLocal = require('passport-local');
