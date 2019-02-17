49ee7edc5614ea2226f02e8ca67a8d7297c68c58
ldaws003 <ldaws003@fiu.edu>
2019-02-16 20:14:40 -0500

    added beginning portion of server and added helmet to package.json

--- server.js
+++ server.js
@@ -8,4 +8,19 @@ const bcrypt = require('bcrypt');
 const socketio = require('socket.io');
 const passportio = require('passport.socketio');
 const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
 const app = express();
+
+app.set('view engine', 'pug');
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet({
+	frameguard: { action: 'deny' },
+	hidePoweredBy: { setTo: 'PHP 4.2.0'}
+}));
+
+app.route('/')
+   .get(function(req, res){
+	   res.render();	   
+   });
