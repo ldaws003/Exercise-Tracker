4d643fa668909c6bd826f65136e61b83c76aa2bf
ldaws003 <ldaws003@fiu.edu>
2019-02-23 10:07:01 -0500

    added passport serialization and deserialization, and app.listen

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const ObjectID = require('mongodb').ObjectID;
+const mongo = require('mongodb').MongoClient;
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const bcrypt = require('bcrypt');
@@ -41,3 +42,25 @@ app.use(session({
 }));
 
 app.use('/', router);
+
+
+mongo.connect(process.env.DB, (err, db) => {
+	if(err) console.log(err);
+	
+    passport.serializeUser((user, done) => {
+		done(null, user._id);		
+	});
+
+    passport.deserializeUser((id, done) => {
+		db.collections('users').findOne(
+		{_id: new ObjectID(id)},
+		(err, doc) => {
+			done(null, doc);
+		}
+	  );		
+	});
+	
+	app.listen(process.env.PORT || 3000, () => {
+		console.log("Listening on port " + process.env.PORT);
+	});
+});
