044b7e8d19f96b7ec0f68ba35fd7268fb2ed87d4
ldaws003 <ldaws003@fiu.edu>
2019-02-23 09:25:42 -0500

    added the session secret to .env file and fixed spaces in package.json

--- server.js
+++ server.js
@@ -2,14 +2,13 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongodb = require('mongodb');
 const mongoose = require('mongoose');
 const ObjectID = require('mongodb').ObjectID;
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const bcrypt = require('bcrypt');
 const io = require('socket.io');
-const passportio = require('passport.socketio');
+const passport = require('passport.socketio');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const session = require('express-session');
@@ -24,6 +23,8 @@ app.set('view engine', 'pug');
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(helmet({
 	frameguard: { action: 'deny' },
@@ -31,7 +32,7 @@ app.use(helmet({
 }));
 
 app.use(session({
-	secret: 'secret',
+	secret: process.env.SESSION_SECRET,
 	resave: true,
 	saveUninitialized: false,
 	store: new MongoStore({
