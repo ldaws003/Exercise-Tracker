90078398e9abcb24d993d6cec936bf5871cebc55
ldaws003 <ldaws003@fiu.edu>
2019-02-26 06:51:54 -0500

    moved passport authentication to user_authentication.js

--- auth/user_authentication.js
+++ auth/user_authentication.js
@@ -1,5 +1,35 @@
 'use strict'
 
-bcrypt = require('bcrypt');
-mongodb = require('mongodb');
+const bcrypt = require('bcrypt');
+const ObjectID = require('mongodb').ObjectID;
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 
+module.exports = function(app, db){
+	passport.serializeUser((user, done) => {
+		done(null, user._id);		
+	});
+
+    passport.deserializeUser((id, done) => {
+		db.collections('users').findOne(
+		{_id: new ObjectID(id)},
+		(err, doc) => {
+			done(null, doc);
+		}
+	  );		
+	});
+	
+	passport.use(new LocalStrategy(
+		function(username, password, done){
+			//maybe collections
+			db.collection('users').findOne({username: username}, function(err, user){
+				if(err){return done(err);}
+				if(!user){return done(null, false)};
+				if(bcrypt.compareSync(password, user.password)){return done(null, false);}
+				return done(null, user);
+			});
+		}
+	));
+	
+	s
+};
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const bcrypt = require('bcrypt');
 const io = require('socket.io');
-const passport = require('passport.socketio');
+const passportio = require('passport.socketio');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const session = require('express-session');
@@ -50,31 +50,6 @@ app.use('/', router);
 
 mongo.connect(process.env.DB, (err, db) => {
 	if(err) console.log(err);
-	
-    passport.serializeUser((user, done) => {
-		done(null, user._id);		
-	});
-
-    passport.deserializeUser((id, done) => {
-		db.collections('users').findOne(
-		{_id: new ObjectID(id)},
-		(err, doc) => {
-			done(null, doc);
-		}
-	  );		
-	});
-	
-	passport.use(new LocalStrategy(
-		function(username, password, done){
-			//maybe collections
-			db.collection('users').findOne({username: username}, function(err, user){
-				if(err){return done(err);}
-				if(!user){return done(null, false)};
-				if(bcrypt.compareSync(password, user.password)){return done(null, false);}
-				return done(null, user);
-			});
-		}
-	));
 	
 	app.route('/register')
 	   .post((req, res, next) => {
