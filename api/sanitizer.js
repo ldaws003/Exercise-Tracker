e05ea586de91face8976a4fd4d8acd6ac91ac712
ldaws003 <ldaws003@fiu.edu>
2019-02-27 17:41:17 -0500

    added in sanitzer.js to remove unwanted information in entries

---
+++ api/sanitizer.js
@@ -0,0 +1,19 @@
+'use strict'
+
+
+//this would sanitize entries and send an error for usernames and passwords when using illegal characters
+function Sanitizer(){
+	
+	//sanitizing passwords
+	this.password = function(){
+		
+	};
+	
+	//sanitizing usernames	
+	this.entry = function(){
+		
+	};
+}
+
+
+module.exports = Sanitizer;
